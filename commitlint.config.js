ea6c348a7dccd5386f309d0fc20154eb1c0cce25
Kaity A <kaity@atikayda.au>
2025-04-15 19:04:43 +1000

    ci: add commit lint changes to allow merge commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -16,6 +16,16 @@ module.exports = {
         'perf',     // Performance improvements
         'revert'    // Reverting changes
       ]
-    ]
-  }
+    ],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [0],
+    'type-case': [2, 'always', ['lower-case']],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always']
+  },
+  ignores: [
+    (commit) => commit.startsWith('Merge remote-tracking branch')
+  ]
 }; 
